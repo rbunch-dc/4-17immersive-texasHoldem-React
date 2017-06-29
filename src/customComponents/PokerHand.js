2e81190fbe8b46d82579460459145d81e92cbc89
Robert Bunch <rob@digitalcrafts.com>
2017-06-29 09:54:56 -0400

    Add deck

---
+++ src/customComponents/PokerHand.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PokerHand extends Component{
+	render(){
+		return(
+			<div className="col-sm-12 poker-hand">
+				A poker hand here!
+			</div>
+		)
+	}
+}
+
+export default PokerHand;
--- src/customComponents/PokerTable.js
+++ src/customComponents/PokerTable.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
+import PokerHand from './PokerHand'
+
+import Deck from '../utilityClasses/Deck'
+
+var cards = new Deck()
 
 class PokerTable extends Component{
+
+	prepDeck(){
+		cards.createDeck();
+		cards.shuffleDeck();
+	}
+
 	render(){
+		this.prepDeck()
+		console.log(cards.deck)
+
 		return(
-			<div>Poker Table here!</div>
+			<div className="col-sm-12 the-table">
+				{ /* <DealerHand /> */ }
+				{ /* <PlayersHand /> */ }
+				<PokerHand /> { /* The computers hand */ }
+				<PokerHand /> { /* Community Cards */ }
+				<PokerHand /> { /* The players hand */ }
+			</div>
 		)
 	}
 }
